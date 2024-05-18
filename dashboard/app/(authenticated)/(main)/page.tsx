30a33f85777f0fda8d6f34d3eb074ac2ea275b39
David | Wolf Worldwide <88748661+wolfworldwidedigital@users.noreply.github.com>
2024-05-18 19:22:57 +0200

    deleted:    .all-contributorsrc

--- dashboard/app/(authenticated)/(main)/page.tsx
+++ dashboard/app/(authenticated)/(main)/page.tsx
@@ -18,12 +18,12 @@ export default async function HomePage() {
         <div className="flex flex-1 items-center justify-between">
           <div>
             <h1 className="text-lg font-bold text-accent-foreground">
-              SAS Copilots ✨
+            Cusmato Copilots ✨
             </h1>
           </div>
           <div className="space-x-2">
             <Button asChild>
-              <Link href="/create/copilot">Create SAS Assistant</Link>
+              <Link href="/create/copilot">Create Cusmato Assistant</Link>
             </Button>
           </div>
         </div>
@@ -33,9 +33,10 @@ export default async function HomePage() {
         <ClosableDivProvider>
           <ClosableDiv className="group relative mb-2 w-full overflow-hidden rounded-lg bg-primary/20 px-8 py-6 before:absolute before:-bottom-5 before:right-14 before:-z-10 before:aspect-square before:h-full before:w-auto before:-rotate-45 before:bg-[url('/rocket_silver.svg')] before:bg-no-repeat before:opacity-60">
             <div className="flex items-center justify-between gap-5">
+              </div>
               <div>
                 <h1 className="text-lg font-bold text-accent-foreground">
-                  Learn OpenCopilot with video tutorials
+                  Learn Cusmato with video tutorials
                 </h1>
             </div>
             <CloseBtn className="absolute -right-1 -top-1 rounded-full border border-border bg-white p-1 opacity-0 shadow group-hover:opacity-100 ">
